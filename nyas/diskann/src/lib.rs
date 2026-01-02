f41cbd4371f7c0aef273c7465ab60b544fa3e3c6
Manish Kumar <manish@neocraft.tech>
2026-01-02 13:13:08 +0530

    Improved SSD index

--- nyas/diskann/src/lib.rs
+++ nyas/diskann/src/lib.rs
@@ -2,5 +2,6 @@ pub(crate) mod disk_index_storage;
 pub(crate) mod in_disk_index;
 pub(crate) mod in_mem_index;
 pub mod index_view;
+pub(crate) mod pq_loader;
 pub(crate) mod product_quantizer;
 pub(crate) mod utils;
